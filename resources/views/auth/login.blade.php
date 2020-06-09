3ca417f352d34bd298f7ea85fff3e6aea6fb7470
afafridaoui <afafridaoui@gmail.com>
2020-06-09 19:20:18 +0100

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card-group">
+            <div class="card p-4">
+                <div class="card-body">
+                    @if(\Session::has('message'))
+                        <p class="alert alert-info">
+                            {{ \Session::get('message') }}
+                        </p>
+                    @endif
+                    
+                    
+                    {{-- <a class="navbar-brand" href="#"><img src={{asset('img/logo.png')}} alt="logo"></a> --}}
+
+                    <form method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+                        <h1>Orzoqni Al Hayat</h1>
+                        <p class="text-muted">{{ trans('global.login') }}</p>
+                        <div style="width: 100%;text-align: right;">
+                            <img src={{asset('img/logoLogin.png')}} style="width: 600px;" />
+                        </div>
+                        <br>
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fa fa-user"></i>
+                                </span>
+                            </div>
+                            <input name="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autofocus placeholder="{{ trans('global.login_email') }}" value="{{ old('email', null) }}">
+                            @if($errors->has('email'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                            </div>
+                            <input name="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required placeholder="{{ trans('global.login_password') }}">
+                            @if($errors->has('password'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('password') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="input-group mb-4">
+                            <div class="form-check checkbox">
+                                <input class="form-check-input" name="remember" type="checkbox" id="remember" style="vertical-align: middle;" />
+                                <label class="form-check-label" for="remember" style="vertical-align: middle;">
+                                    {{ trans('global.remember_me') }}
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <button type="submit" class="btn btn-danger px-4">
+                                    {{ trans('global.login') }}
+                                </button>
+                            </div>
+                            <div class="col-6 text-right">
+                                <a class="btn btn-link text-success px-0" href="{{ route('password.request') }}">
+                                    {{ trans('global.forgot_password') }}
+                                </a>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
