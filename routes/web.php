8934555a0f24b32baa5021bb0b45d50b90a9b9b5
afafridaoui <afafridaoui@gmail.com>
2020-06-14 19:34:51 +0100

    Added Post Managing requires

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::redirect('/', '/login');
 Route::redirect('/home', '/admin');
 Auth::routes(['register' => false]);
